fee8c4d58c5c9665fdfed73289e62c8b15438f7f
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2025-09-22 06:51:00 +0700

    Enable Flatpickr date picker with Indonesian locale
    Add styling and configuration for single date selection with Indonesian
    language support and custom date format

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,9 @@
 // import "jsvectormap/dist/jsvectormap.min.css";
-// import "flatpickr/dist/flatpickr.min.css";
+import "flatpickr/dist/flatpickr.min.css";
 // import "dropzone/dist/dropzone.css";
 
-// import flatpickr from "flatpickr";
+import flatpickr from "flatpickr";
+import { Indonesian } from "flatpickr/dist/l10n/id";
 // import Dropzone from "dropzone";
 
 // import { Livewire } from "../../vendor/livewire/livewire/dist/livewire.esm";
@@ -17,27 +18,28 @@ import chart01 from "./components/charts/chart-01";
 // Livewire.start();
 
 // // Init flatpickr
-// flatpickr(".datepicker", {
-//   mode: "range",
-//   static: true,
-//   monthSelectorType: "static",
-//   dateFormat: "M j, Y",
-//   defaultDate: [new Date().setDate(new Date().getDate() - 6), new Date()],
-//   prevArrow:
-//     '<svg class="stroke-current" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.25 6L9 12.25L15.25 18.5" stroke="" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg>',
-//   nextArrow:
-//     '<svg class="stroke-current" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.75 19L15 12.75L8.75 6.5" stroke="" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg>',
-//   onReady: (selectedDates, dateStr, instance) => {
-//     // eslint-disable-next-line no-param-reassign
-//     instance.element.value = dateStr.replace("to", "-");
-//     const customClass = instance.element.getAttribute("data-class");
-//     instance.calendarContainer.classList.add(customClass);
-//   },
-//   onChange: (selectedDates, dateStr, instance) => {
-//     // eslint-disable-next-line no-param-reassign
-//     instance.element.value = dateStr.replace("to", "-");
-//   },
-// });
+flatpickr(".datepicker", {
+  locale: Indonesian,
+  mode: "single",
+  static: true,
+  monthSelectorType: "static",
+  dateFormat: "l, j F Y",
+  defaultDate: [new Date()],
+  prevArrow:
+    '<svg class="stroke-current" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.25 6L9 12.25L15.25 18.5" stroke="" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg>',
+  nextArrow:
+    '<svg class="stroke-current" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.75 19L15 12.75L8.75 6.5" stroke="" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg>',
+  onReady: (selectedDates, dateStr, instance) => {
+    // eslint-disable-next-line no-param-reassign
+    instance.element.value = dateStr.replace("to", "-");
+    const customClass = instance.element.getAttribute("data-class");
+    instance.calendarContainer.classList.add(customClass);
+  },
+  onChange: (selectedDates, dateStr, instance) => {
+    // eslint-disable-next-line no-param-reassign
+    instance.element.value = dateStr.replace("to", "-");
+  },
+});
 
 // // Init Dropzone
 // const dropzoneArea = document.querySelectorAll("#demo-upload");
