bfba702cbacde14765f333bf43f202140d4c3628
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2025-09-24 21:59:42 +0700

    Create activities table migration

---
+++ database/migrations/2025_09_21_223104_create_activities_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('title');
+            $table->string('type');
+            $table->date('start_date');
+            $table->text('description');
+            $table->boolean('published');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('activities');
+    }
+};
