8395ff7bdf7939c849efbdd73689005b01150782
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2025-09-24 21:40:25 +0700

    Add activity routes and update dashboard view path

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Activity;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
@@ -11,8 +12,12 @@ Route::middleware('guest')->group(function () {
 
 Route::middleware('auth')->group(function () {
     Route::prefix('admin')->group(function () {
-        Route::get('/dashboard', fn () => view('dashboard'))->name('dashboard');
+        Route::get('/dashboard', fn () => view('admin.dashboard'))->name('dashboard');
 
+        Route::prefix('kegiatan')->group(function () {
+            Route::get('/', fn () => view('admin.kegiatan.index', ['activities' => Activity::all()]))->name('kegiatan.index');
+            Route::get('/tambah', fn () => view('admin.kegiatan.create'))->name('kegiatan.create');
+        });
     });
 
     Route::post('logout', [AuthController::class, 'logout'])->name('logout');
