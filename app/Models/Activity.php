457295847b426d615c05e8db54c199db43a6fe5f
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2025-09-26 01:56:49 +0700

    Use auto-incrementing ID and add slug for activities

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -2,19 +2,12 @@
 
 namespace App\Models;
 
-use Ramsey\Uuid\Uuid;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Activity extends Model
 {
-    use HasFactory, HasUuids;
+    use HasFactory;
 
     protected $guarded = ['id'];
-
-    public function newUniqueId(): string
-    {
-        return (string) Uuid::uuid4();
-    }
 }
--- database/migrations/2025_09_21_223104_create_activities_table.php
+++ database/migrations/2025_09_21_223104_create_activities_table.php
@@ -12,8 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('activities', function (Blueprint $table) {
-            $table->uuid('id')->primary();
+            $table->id();
             $table->string('title');
+            $table->string('slug')->unique();
             $table->string('type');
             $table->date('start_date');
             $table->text('description');
