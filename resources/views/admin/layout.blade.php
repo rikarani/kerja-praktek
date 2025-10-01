6491f81ba239c34888d09d221df7a64fe0e65500
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2025-10-01 22:08:30 +0700

    formatter

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -1,15 +1,15 @@
 @extends('root')
 
 @section('content')
-    <div class="flex h-screen overflow-hidden">
-        @include('sidebar')
-        <div class="relative flex flex-col flex-1 overflow-x-hidden overflow-y-auto">
-            @include('partials.header')
-            <main>
-                <div class="p-4 mx-auto max-w-(--breakpoint-2xl) md:p-6">
-                    @yield('main-content')
-                </div>
-            </main>
+  <div class="flex h-screen overflow-hidden">
+    @include('sidebar')
+    <div class="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+      @include('partials.header')
+      <main>
+        <div class="max-w-(--breakpoint-2xl) mx-auto p-4 md:p-6">
+          @yield('main-content')
         </div>
+      </main>
     </div>
+  </div>
 @endsection
