179f3ed615ecfc027c919370ff7af70b800351fa
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2025-10-01 22:10:57 +0700

    formatter

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,23 +1,23 @@
 @extends('root')
 
 @section('content')
-    <div class="flex h-screen overflow-hidden">
-        @include('sidebar')
-        <div class="relative flex flex-col flex-1 overflow-x-hidden overflow-y-auto">
-            @include('partials.header')
-            <main>
-                <div class="p-4 mx-auto max-w-(--breakpoint-2xl) md:p-6">
-                    <div class="grid grid-cols-12 gap-4 md:gap-6">
-                        <div class="col-span-12 space-y-6 xl:col-span-7">
-                            <!-- Metric Group One -->
-                            @include('partials.metric-group.metric-group-01')
-                            <!-- Metric Group One -->
+  <div class="flex h-screen overflow-hidden">
+    @include('sidebar')
+    <div class="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+      @include('partials.header')
+      <main>
+        <div class="max-w-(--breakpoint-2xl) mx-auto p-4 md:p-6">
+          <div class="grid grid-cols-12 gap-4 md:gap-6">
+            <div class="col-span-12 space-y-6 xl:col-span-7">
+              <!-- Metric Group One -->
+              @include('partials.metric-group.metric-group-01')
+              <!-- Metric Group One -->
 
-                            <!-- ====== Chart One Start -->
-                            @include('partials.chart.chart-01')
-                            <!-- ====== Chart One End -->
-                        </div>
-                        {{-- <div class="col-span-12 xl:col-span-5">
+              <!-- ====== Chart One Start -->
+              @include('partials.chart.chart-01')
+              <!-- ====== Chart One End -->
+            </div>
+            {{-- <div class="col-span-12 xl:col-span-5">
                           <!-- ====== Chart Two Start -->
                           <include src="./partials/chart/chart-02.html" />
                           <!-- ====== Chart Two End -->
@@ -40,9 +40,9 @@
                           <include src="./partials/table/table-01.html" />
                           <!-- ====== Table One End -->
                       </div> --}}
-                    </div>
-                </div>
-            </main>
+          </div>
         </div>
+      </main>
     </div>
+  </div>
 @endsection
