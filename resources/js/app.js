10a6297af21edd37d03701e6628822ea3c65d10f
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2025-10-15 02:32:50 +0700

    Remove unused chart import and initialization

--- resources/js/app.js
+++ resources/js/app.js
@@ -28,8 +28,6 @@ import { Arrows } from "@fancyapps/ui/dist/carousel/carousel.arrows";
 import { Autoplay } from "@fancyapps/ui/dist/carousel/carousel.autoplay";
 import { Lazyload } from "@fancyapps/ui/dist/carousel/carousel.lazyload";
 
-import chart01 from "./components/charts/chart-01";
-
 window.setupEditor = (content) => {
   const CustomBold = Bold.extend({
     renderHTML({ mark, HTMLAttributes }) {
@@ -145,11 +143,6 @@ flatpickr(".datepicker", {
     '<svg class="stroke-current" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.25 6L9 12.25L15.25 18.5" stroke="" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg>',
   nextArrow:
     '<svg class="stroke-current" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.75 19L15 12.75L8.75 6.5" stroke="" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg>',
-});
-
-// // Document Loaded
-document.addEventListener("DOMContentLoaded", () => {
-  chart01();
 });
 
 if (document.getElementById("myCarousel")) {
