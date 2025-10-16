4ae4901fed08977a9a42a17d7d8b480eec116714
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2025-10-16 08:53:27 +0700

    delete comment

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,9 +3,7 @@
 @section('main-content')
   <div class="grid grid-cols-12 gap-4 md:gap-6">
     <div class="col-span-12 space-y-6 xl:col-span-7">
-      <!-- ====== Chart One Start -->
       <livewire:admin.dashboard.chart />
-      <!-- ====== Chart One End -->
     </div>
   </div>
 @endsection
