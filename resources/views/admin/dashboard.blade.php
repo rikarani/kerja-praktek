ae0fe78e865c99f4174c8d1a71a8adefdfafe1fd
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2025-10-16 08:40:06 +0700

    delete metrics component

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,10 +3,6 @@
 @section('main-content')
   <div class="grid grid-cols-12 gap-4 md:gap-6">
     <div class="col-span-12 space-y-6 xl:col-span-7">
-      <!-- Metric Group One -->
-      @include('partials.metric-group.metric-group-01')
-      <!-- Metric Group One -->
-
       <!-- ====== Chart One Start -->
       <livewire:admin.dashboard.chart />
       <!-- ====== Chart One End -->
