043c5d2e218ca7fa3e5a8e2c2c1a6081893694a4
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2025-10-18 05:39:13 +0700

    refactor: simplify select class in index.blade.php

--- resources/views/livewire/admin/activity/index.blade.php
+++ resources/views/livewire/admin/activity/index.blade.php
@@ -22,7 +22,7 @@
       </div>
       <div class="relative z-20 bg-transparent" wire:ignore>
         <select
-          class="{{ twMerge('disabled:opacity-50 shadow-theme-xs w-full border border-gray-300 dark:border-gray-700 dark:bg-gray-900 focus:border-brand-300 focus:ring-brand-500/10 dark:focus:border-brand-800 rounded-lg bg-transparent px-4 py-2.5 pr-10 appearance-none text-sm text-gray-800 placeholder:text-gray-400 focus:ring-3 focus:outline-hidden dark:text-white/90 dark:placeholder:text-white/30', $errors->has('type') ? 'border-error-300 focus:border-error-300 focus:ring-error-500/10 dark:border-error-700 dark:focus:border-error-800' : '') }}"
+          class="shadow-theme-xs focus:border-brand-300 focus:ring-brand-500/10 dark:focus:border-brand-800 focus:ring-3 focus:outline-hidden w-full appearance-none rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 pr-10 text-sm text-gray-800 placeholder:text-gray-400 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30"
           required wire:model.live="bulan">
           <option class="text-gray-700 dark:bg-gray-900 dark:text-gray-400" value="" selected>
             Semua Bulan
