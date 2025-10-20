8ad399b25012aa3c9d984e03c5a6c5c6ab4aab6c
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2025-10-20 13:21:05 +0700

    feat: add update category modal with form handling and validation

---
+++ app/Livewire/Modal/Admin/Category/Update.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Livewire\Modal\Admin\Category;
+
+use Livewire\Component;
+use App\Models\Category;
+use Livewire\Attributes\On;
+use Illuminate\Validation\Rule;
+use Illuminate\Contracts\View\View;
+
+class Update extends Component
+{
+    public ?Category $category = null;
+
+    public string $name = '';
+
+    #[On('update-category')]
+    public function prepare(Category $category): void
+    {
+        $this->category = $category;
+        $this->fill($this->category);
+
+        $this->dispatch('open-modal', modal: 'update-category');
+    }
+
+    public function submit(): void
+    {
+        $data = $this->validate();
+
+        $this->category->update($data);
+
+        $this->dispatch('close-modal', modal: 'update-category');
+        $this->redirectRoute('category.index');
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', Rule::unique('categories', 'name')->ignore($this?->category)],
+        ];
+    }
+
+    public function render(): View
+    {
+        return view('livewire.modal.admin.category.update');
+    }
+}
