74a5b4daaa4144d202de90af1aef4d5fe08ea8b6
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2025-10-20 21:28:52 +0700

    feat: update activity display to show category name instead of type

--- resources/views/livewire/activity/index.blade.php
+++ resources/views/livewire/activity/index.blade.php
@@ -26,7 +26,7 @@
                   d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z">
                 </path>
               </svg>
-              {{ Str::ucfirst($activity->type) }}
+              {{ Str::ucfirst($activity->category->name) }}
             </span>
             <span class="text-sm">{{ $activity->start_date->diffForHumans() }}</span>
           </div>
