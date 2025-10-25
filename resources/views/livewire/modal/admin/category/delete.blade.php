a6ea6e0678ce8b9170fea137d1fd4a69116848f7
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2025-10-26 02:40:15 +0700

    feat: improve delete confirmation UI by removing unnecessary elements

--- resources/views/livewire/modal/admin/category/delete.blade.php
+++ resources/views/livewire/modal/admin/category/delete.blade.php
@@ -7,7 +7,6 @@
           d="M34.364 6.85053C38.6205 -2.28351 51.3795 -2.28351 55.636 6.85053C58.0129 11.951 63.5594 14.6722 68.9556 13.3853C78.6192 11.0807 86.5743 21.2433 82.2185 30.3287C79.7862 35.402 81.1561 41.5165 85.5082 45.0122C93.3019 51.2725 90.4628 63.9451 80.7747 66.1403C75.3648 67.3661 71.5265 72.2695 71.5572 77.9156C71.6123 88.0265 60.1169 93.6664 52.3918 87.3184C48.0781 83.7737 41.9219 83.7737 37.6082 87.3184C29.8831 93.6664 18.3877 88.0266 18.4428 77.9156C18.4735 72.2695 14.6352 67.3661 9.22531 66.1403C-0.462787 63.9451 -3.30193 51.2725 4.49185 45.0122C8.84391 41.5165 10.2138 35.402 7.78151 30.3287C3.42572 21.2433 11.3808 11.0807 21.0444 13.3853C26.4406 14.6722 31.9871 11.951 34.364 6.85053Z"
           fill="" fill-opacity=""></path>
       </svg>
-
       <span class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
         <svg class="fill-error-600 dark:fill-error-500" width="38" height="38" viewBox="0 0 38 38" fill="none"
           xmlns="http://www.w3.org/2000/svg">
@@ -17,14 +16,12 @@
         </svg>
       </span>
     </div>
-
     <h4 class="sm:text-title-sm mb-2 text-2xl font-semibold text-gray-800 dark:text-white/90">
       Konfirmasi Hapus
     </h4>
     <p class="text-sm leading-6 text-gray-500 dark:text-gray-400">
       Yakin mau hapus kategori <span class="font-semibold">{{ $category?->name }}</span>?
     </p>
-
     <div class="mt-7 flex w-full items-center justify-center gap-3">
       <button
         class="bg-error-500 shadow-theme-xs hover:bg-error-600 flex w-full justify-center gap-2 rounded-lg px-4 py-3 text-sm font-medium text-white sm:w-auto"
