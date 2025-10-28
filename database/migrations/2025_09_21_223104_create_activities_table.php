0f00b79db8b167f128ce17a518218d04dca19946
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2025-10-28 17:19:49 +0700

    feat: update activities table to use UUID for primary key

--- database/migrations/2025_09_21_223104_create_activities_table.php
+++ database/migrations/2025_09_21_223104_create_activities_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('activities', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->string('title');
             $table->string('slug')->unique();
             $table->text('excerpt');
