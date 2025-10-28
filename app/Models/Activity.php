b19a6bcab7a1d5d12074b0e6d2c96a93c121ba8b
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2025-10-28 17:20:09 +0700

    feat: add user relation to activities table and enable UUIDs for primary key

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -6,11 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
 use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Attributes\Scope;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Activity extends Model
 {
-    use Sluggable;
+    use HasUuids, Sluggable;
 
     protected $guarded = ['id'];
 
@@ -33,6 +34,11 @@ class Activity extends Model
         return $this->belongsTo(Category::class);
     }
 
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
     #[Scope]
     protected function search(Builder $query, string $keyword): Builder
     {
@@ -46,13 +52,13 @@ class Activity extends Model
     }
 
     #[Scope]
-    protected function bulan(Builder $query, ?int $bulan): Builder
+    protected function month(Builder $query, ?int $bulan): Builder
     {
         return $query->when($bulan, fn (Builder $q) => $q->whereMonth('start_date', $bulan));
     }
 
     #[Scope]
-    protected function tahun(Builder $query, ?string $tahun): Builder
+    protected function year(Builder $query, ?string $tahun): Builder
     {
         return $query->when($tahun, fn (Builder $q) => $q->whereYear('start_date', $tahun));
     }
