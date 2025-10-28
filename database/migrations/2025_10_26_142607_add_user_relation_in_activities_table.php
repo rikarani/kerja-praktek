900bd4f3268a2aa6b749687603c0938f6667b083
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2025-10-28 17:17:45 +0700

    feat: add user relation to activities table for improved data association

---
+++ database/migrations/2025_10_26_142607_add_user_relation_in_activities_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('activities', function (Blueprint $table) {
+            $table->foreignUuid('user_id')->after('category_id')->constrained()->cascadeOnDelete();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('activities', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropColumn('user_id');
+        });
+    }
+};
