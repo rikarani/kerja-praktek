35021b2ae0aa0f02a1e9947026684ea917de46fa
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2025-10-28 17:17:53 +0700

    feat: add role relation to users table for improved role management

---
+++ database/migrations/2025_10_26_185028_add_role_relation_in_users_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignUuid('role_id')->after('id')->constrained()->cascadeOnDelete();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['role_id']);
+            $table->dropColumn('role_id');
+        });
+    }
+};
