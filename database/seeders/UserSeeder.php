1b62cca62990ae19e9bf4e7f59c1a316499302be
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2025-11-02 06:17:20 +0700

    feat: add default admin and regular users to UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,13 +2,32 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Config;
 
 class UserSeeder extends Seeder
 {
     public function run(): void
     {
+        // Default Admin User
+        User::create([
+            'role_id' => Role::where('name', 'admin')->first()->id,
+            'name' => 'Example Admin',
+            'email' => fake()->unique()->safeEmail(),
+            'password' => Hash::make(Config::get('app.secret')),
+        ]);
+
+        // Default Regular Users
+        User::create([
+            'role_id' => Role::where('name', 'user')->first()->id,
+            'name' => 'Example User 1',
+            'email' => fake()->unique()->safeEmail(),
+            'password' => Hash::make(Config::get('app.secret')),
+        ]);
+
         User::factory(15)->create();
     }
 }
