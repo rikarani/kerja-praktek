5476f910d69fb0807d70cbabc2d2525438878e0f
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2025-11-08 03:50:24 +0700

    fix: add login button to index page for improved user access

--- resources/views/activity/index.blade.php
+++ resources/views/activity/index.blade.php
@@ -2,8 +2,14 @@
 
 @section('content')
   <section class="min-h-dvh bg-white dark:bg-gray-900">
-    <div class="mx-auto max-w-screen-xl px-4 py-8 lg:px-6 lg:py-16">
-      <div class="mx-auto mb-8 max-w-screen-sm text-center lg:mb-16">
+    <div class="mx-auto max-w-screen-xl px-4 py-8 lg:px-6 lg:py-8">
+      <div class="justify-end-safe flex">
+        <a class="bg-brand-500 shadow-theme-xs hover:bg-brand-600 inline-flex items-center gap-2 rounded-lg px-4 py-3 text-sm font-medium text-white transition"
+          href="{{ route('login') }}">
+          Login
+        </a>
+      </div>
+      <div class="mx-auto mb-8 mt-4 max-w-screen-sm text-center lg:mb-16">
         <h2 class="mb-4 text-3xl font-extrabold tracking-tight text-gray-900 lg:text-4xl dark:text-white">
           Dokumentasi Kegiatan
         </h2>
