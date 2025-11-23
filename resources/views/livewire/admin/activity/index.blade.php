fa40cd4ec9f114ff093b11a6588a6920fa52cfc8
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2025-11-24 00:02:09 +0700

    fix: enhance activity table with additional details and action buttons

--- resources/views/livewire/admin/activity/index.blade.php
+++ resources/views/livewire/admin/activity/index.blade.php
@@ -1,3 +1,5 @@
+@use('App\Support\Helper')
+
 <div
   class="overflow-hidden rounded-2xl border border-gray-200 bg-white px-4 pb-3 pt-4 sm:px-6 dark:border-gray-800 dark:bg-white/[0.03]">
   <div class="mb-4 flex flex-col gap-2 sm:flex-row sm:items-center sm:justify-between">
@@ -84,7 +86,7 @@
     @else
       <table class="min-w-full">
         <thead>
-          <tr class="border-y border-gray-100 dark:border-gray-800">
+          <tr class="max-w-max border-y border-gray-100 dark:border-gray-800">
             <th class="py-3">
               <div class="flex items-center">
                 <p class="text-theme-xs font-medium text-gray-500 dark:text-gray-400">
@@ -109,6 +111,13 @@
             <th class="py-3">
               <div class="flex items-center">
                 <p class="text-theme-xs font-medium text-gray-500 dark:text-gray-400">
+                  Jumlah Dokumentasi
+                </p>
+              </div>
+            </th>
+            <th class="py-3">
+              <div class="flex items-center">
+                <p class="text-theme-xs font-medium text-gray-500 dark:text-gray-400">
                   Status Publikasi
                 </p>
               </div>
@@ -126,16 +135,14 @@
           @foreach ($activities as $activity)
             <tr wire:key="{{ $activity->slug }}">
               <td class="py-3">
-                <div class="flex items-center">
-                  <div class="flex items-center gap-3">
-                    <div class="h-10 w-10 overflow-hidden rounded-full">
-                      <img src="{{ asset('images/user/user-17.jpg') }}" alt="brand" />
-                    </div>
-                    <div>
-                      <span class="text-theme-sm block font-medium text-gray-800 dark:text-white/90">
-                        {{ $activity->author->name }}
-                      </span>
-                    </div>
+                <div class="flex max-w-max items-center gap-3">
+                  <div class="h-10 w-10 overflow-hidden rounded-full">
+                    <img src="{{ asset('images/user/user-17.jpg') }}" alt="brand" />
+                  </div>
+                  <div>
+                    <span class="text-theme-sm block font-medium text-gray-800 dark:text-white/90">
+                      {{ $activity->author->name }}
+                    </span>
                   </div>
                 </div>
               </td>
@@ -144,7 +151,7 @@
                   <div class="flex items-center gap-3">
                     <div>
                       <p class="text-theme-sm font-medium text-gray-800 dark:text-white/90">
-                        {{ Str::limit($activity->title, 25) }}
+                        {{ Str::limit($activity->title, 50) }}
                       </p>
                       <span class="text-theme-xs text-gray-500 dark:text-gray-400">
                         {{ $activity->start_date->translatedFormat('d F Y') }}
@@ -157,6 +164,16 @@
                 <div class="flex items-center">
                   <p class="text-theme-sm text-gray-500 dark:text-gray-400">
                     {{ Str::ucfirst($activity->category->name) }}
+                  </p>
+                </div>
+              </td>
+              <td class="py-3">
+                <div class="">
+                  <p class="text-theme-sm text-gray-500 dark:text-gray-400">
+                    {{ Helper::getPhotoCount($activity) }} Foto
+                  </p>
+                  <p class="text-theme-sm text-gray-500 dark:text-gray-400">
+                    {{ Helper::getVideoCount($activity) }} Video
                   </p>
                 </div>
               </td>
@@ -175,27 +192,67 @@
                   @endif
                 </div>
               </td>
-              <td class="flex gap-3 py-3">
-                <a class="bg-blue-light-500 hover:bg-blue-light-600 shadow-theme-xs inline-flex items-center justify-center gap-2 rounded-lg border border-gray-300 px-4 py-2.5 text-white dark:border-gray-700"
-                  href="{{ route('activity.preview', ['activity' => $activity]) }}">
-                  <svg class="size-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                    stroke-width="1.5" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                      d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                  </svg>
-                  <span>Preview</span>
-                </a>
-                <button
-                  class="bg-error-500 hover:bg-error-600 shadow-theme-xs inline-flex items-center justify-center gap-2 rounded-lg border border-gray-300 px-4 py-2.5 text-white dark:border-gray-700"
-                  type="button" @click="$dispatch('delete-activity', { activity: '{{ $activity->slug }}'} )">
-                  <svg class="size-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                    stroke-width="1.5" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                      d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                  </svg>
-                  <span>Hapus</span>
-                </button>
+              <td class="flex gap-1.5 py-3">
+                <div class="group relative inline-block">
+                  <a class="bg-brand-500 hover:bg-brand-600 shadow-theme-xs inline-flex items-center justify-center gap-2 rounded-lg border border-gray-300 px-4 py-2.5 text-white dark:border-gray-700"
+                    href="{{ route('admin.activity.detail', ['activity' => $activity]) }}">
+                    <svg class="size-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                      stroke-width="1.5" stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round"
+                        d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+                  </a>
+                  <span
+                    class="pointer-events-none absolute left-0 top-1/2 mr-2 max-w-max -translate-x-full -translate-y-1/2 rounded-md bg-gray-700 px-3 py-1 text-sm text-white opacity-0 shadow-lg transition-opacity duration-200 group-hover:opacity-100">
+                    Detail
+                  </span>
+                </div>
+                <div class="group relative inline-block">
+                  <a class="bg-blue-light-500 hover:bg-blue-light-600 shadow-theme-xs inline-flex items-center justify-center gap-2 rounded-lg border border-gray-300 px-4 py-2.5 text-white dark:border-gray-700"
+                    href="{{ route('admin.activity.preview', ['activity' => $activity]) }}">
+                    <svg class="size-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                      stroke-width="1.5" stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                    </svg>
+                  </a>
+                  <span
+                    class="pointer-events-none absolute left-0 top-1/2 mr-2 max-w-max -translate-x-full -translate-y-1/2 rounded-md bg-gray-700 px-3 py-1 text-sm text-white opacity-0 shadow-lg transition-opacity duration-200 group-hover:opacity-100">
+                    Preview
+                  </span>
+                </div>
+                @if ($activity->published)
+                  <div class="group relative inline-block">
+                    <button
+                      class="bg-warning-500 hover:bg-warning-600 shadow-theme-xs inline-flex items-center justify-center gap-2 rounded-lg border border-gray-300 px-4 py-2.5 text-white dark:border-gray-700"
+                      type="button" @click="$dispatch('unpublish-activity', { activity: '{{ $activity->slug }}'} )">
+                      <svg class="size-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                        stroke-width="1.5" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                      </svg>
+                    </button>
+                    <span
+                      class="pointer-events-none absolute left-0 top-1/2 mr-2 max-w-max -translate-x-full -translate-y-1/2 rounded-md bg-gray-700 px-3 py-1 text-sm text-white opacity-0 shadow-lg transition-opacity duration-200 group-hover:opacity-100">
+                      Unpublish
+                    </span>
+                  </div>
+                @else
+                  <div class="group relative inline-block">
+                    <button
+                      class="bg-success-500 hover:bg-success-600 shadow-theme-xs inline-flex items-center justify-center gap-2 rounded-lg border border-gray-300 px-4 py-2.5 text-white dark:border-gray-700"
+                      type="button" @click="$dispatch('publish-activity', { activity: '{{ $activity->slug }}'} )">
+                      <svg class="size-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                        stroke-width="1.5" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+                      </svg>
+                    </button>
+                    <span
+                      class="pointer-events-none absolute left-0 top-1/2 mr-2 max-w-max -translate-x-full -translate-y-1/2 rounded-md bg-gray-700 px-3 py-1 text-sm text-white opacity-0 shadow-lg transition-opacity duration-200 group-hover:opacity-100">
+                      Publish
+                    </span>
+                  </div>
+                @endif
               </td>
             </tr>
           @endforeach
@@ -206,7 +263,8 @@
           {{ $activities->links() }}
         </div>
       @endif
-      <livewire:modal.admin.activity.delete />
+      <livewire:modal.admin.activity.publish />
+      <livewire:modal.admin.activity.unpublish />
     @endif
   </div>
 </div>
