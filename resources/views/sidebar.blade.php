04b9569823db46bad5a7f2d256e03062469a5c2d
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2025-11-26 15:17:07 +0700

    feat: refactor sidebar layout for admin section and user management links

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -54,62 +54,52 @@
                 Daftar Kegiatan
               </span>
             </a>
-            {{--            <ul class="menu-dropdown mt-2 flex flex-col gap-1 pl-9"> --}}
-            {{--              <li> --}}
-            {{--                <a class="menu-dropdown-item {{ Route::is('category.index') ? 'menu-dropdown-item-active' : 'menu-dropdown-item-inactive' }} group" --}}
-            {{--                  href="{{ route('category.index') }}" :class="sidebarToggle ? 'lg:hidden' : 'flex'"> --}}
-            {{--                  Kategori Kegiatan --}}
-            {{--                </a> --}}
-            {{--              </li> --}}
-            {{--            </ul> --}}
           </li>
         </ul>
       </div>
-      @if (Auth::user()->isAdmin())
-        <div>
-          <h3 class="mb-4 text-xs uppercase leading-[20px] text-gray-400">
-            <span class="menu-group-title" :class="sidebarToggle ? 'lg:hidden' : ''">
-              Khusus Admin
-            </span>
-            <svg class="menu-group-icon mx-auto fill-current" :class="sidebarToggle ? 'lg:block hidden' : 'hidden'"
-              width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd" clip-rule="evenodd"
-                d="M5.99915 10.2451C6.96564 10.2451 7.74915 11.0286 7.74915 11.9951V12.0051C7.74915 12.9716 6.96564 13.7551 5.99915 13.7551C5.03265 13.7551 4.24915 12.9716 4.24915 12.0051V11.9951C4.24915 11.0286 5.03265 10.2451 5.99915 10.2451ZM17.9991 10.2451C18.9656 10.2451 19.7491 11.0286 19.7491 11.9951V12.0051C19.7491 12.9716 18.9656 13.7551 17.9991 13.7551C17.0326 13.7551 16.2491 12.9716 16.2491 12.0051V11.9951C16.2491 11.0286 17.0326 10.2451 17.9991 10.2451ZM13.7491 11.9951C13.7491 11.0286 12.9656 10.2451 11.9991 10.2451C11.0326 10.2451 10.2491 11.0286 10.2491 11.9951V12.0051C10.2491 12.9716 11.0326 13.7551 11.9991 13.7551C12.9656 13.7551 13.7491 12.9716 13.7491 12.0051V11.9951Z"
-                fill="" />
-            </svg>
-          </h3>
-          <ul class="mb-6 flex flex-col gap-4">
-            <li>
-              <a class="menu-item {{ Route::is('category.index') ? 'menu-item-active' : 'menu-item-inactive' }} group"
-                href="{{ route('category.index') }}">
-                <svg class="{{ Route::is('category.index') ? 'menu-item-icon-active' : 'menu-item-icon-inactive' }}"
-                  width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path fill-rule="evenodd" clip-rule="evenodd"
-                    d="M8 2C8.41421 2 8.75 2.33579 8.75 2.75V3.75H15.25V2.75C15.25 2.33579 15.5858 2 16 2C16.4142 2 16.75 2.33579 16.75 2.75V3.75H18.5C19.7426 3.75 20.75 4.75736 20.75 6V9V19C20.75 20.2426 19.7426 21.25 18.5 21.25H5.5C4.25736 21.25 3.25 20.2426 3.25 19V9V6C3.25 4.75736 4.25736 3.75 5.5 3.75H7.25V2.75C7.25 2.33579 7.58579 2 8 2ZM8 5.25H5.5C5.08579 5.25 4.75 5.58579 4.75 6V8.25H19.25V6C19.25 5.58579 18.9142 5.25 18.5 5.25H16H8ZM19.25 9.75H4.75V19C4.75 19.4142 5.08579 19.75 5.5 19.75H18.5C18.9142 19.75 19.25 19.4142 19.25 19V9.75Z"
-                    fill="" />
-                </svg>
-                <span class="menu-item-text" :class="sidebarToggle ? 'lg:hidden' : ''">
-                  Kategori Kegiatan
-                </span>
-              </a>
-            </li>
-            <li>
-              <a class="menu-item {{ Route::is('user.index') ? 'menu-item-active' : 'menu-item-inactive' }} group"
-                href="{{ route('user.index') }}">
-                <svg class="{{ Route::is('user.index') ? 'menu-item-icon-active' : 'menu-item-icon-inactive' }}"
-                  width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path fill-rule="evenodd" clip-rule="evenodd"
-                    d="M8 2C8.41421 2 8.75 2.33579 8.75 2.75V3.75H15.25V2.75C15.25 2.33579 15.5858 2 16 2C16.4142 2 16.75 2.33579 16.75 2.75V3.75H18.5C19.7426 3.75 20.75 4.75736 20.75 6V9V19C20.75 20.2426 19.7426 21.25 18.5 21.25H5.5C4.25736 21.25 3.25 20.2426 3.25 19V9V6C3.25 4.75736 4.25736 3.75 5.5 3.75H7.25V2.75C7.25 2.33579 7.58579 2 8 2ZM8 5.25H5.5C5.08579 5.25 4.75 5.58579 4.75 6V8.25H19.25V6C19.25 5.58579 18.9142 5.25 18.5 5.25H16H8ZM19.25 9.75H4.75V19C4.75 19.4142 5.08579 19.75 5.5 19.75H18.5C18.9142 19.75 19.25 19.4142 19.25 19V9.75Z"
-                    fill="" />
-                </svg>
-                <span class="menu-item-text" :class="sidebarToggle ? 'lg:hidden' : ''">
-                  Manajemen User
-                </span>
-              </a>
-            </li>
-          </ul>
-        </div>
-      @endif
+      <div>
+        <h3 class="mb-4 text-xs uppercase leading-[20px] text-gray-400">
+          <span class="menu-group-title" :class="sidebarToggle ? 'lg:hidden' : ''">
+            Khusus Admin
+          </span>
+          <svg class="menu-group-icon mx-auto fill-current" :class="sidebarToggle ? 'lg:block hidden' : 'hidden'"
+            width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" clip-rule="evenodd"
+              d="M5.99915 10.2451C6.96564 10.2451 7.74915 11.0286 7.74915 11.9951V12.0051C7.74915 12.9716 6.96564 13.7551 5.99915 13.7551C5.03265 13.7551 4.24915 12.9716 4.24915 12.0051V11.9951C4.24915 11.0286 5.03265 10.2451 5.99915 10.2451ZM17.9991 10.2451C18.9656 10.2451 19.7491 11.0286 19.7491 11.9951V12.0051C19.7491 12.9716 18.9656 13.7551 17.9991 13.7551C17.0326 13.7551 16.2491 12.9716 16.2491 12.0051V11.9951C16.2491 11.0286 17.0326 10.2451 17.9991 10.2451ZM13.7491 11.9951C13.7491 11.0286 12.9656 10.2451 11.9991 10.2451C11.0326 10.2451 10.2491 11.0286 10.2491 11.9951V12.0051C10.2491 12.9716 11.0326 13.7551 11.9991 13.7551C12.9656 13.7551 13.7491 12.9716 13.7491 12.0051V11.9951Z"
+              fill="" />
+          </svg>
+        </h3>
+        <ul class="mb-6 flex flex-col gap-4">
+          <li>
+            <a class="menu-item {{ Route::is('category.index') ? 'menu-item-active' : 'menu-item-inactive' }} group"
+              href="{{ route('category.index') }}">
+              <svg class="{{ Route::is('category.index') ? 'menu-item-icon-active' : 'menu-item-icon-inactive' }}"
+                width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd"
+                  d="M8 2C8.41421 2 8.75 2.33579 8.75 2.75V3.75H15.25V2.75C15.25 2.33579 15.5858 2 16 2C16.4142 2 16.75 2.33579 16.75 2.75V3.75H18.5C19.7426 3.75 20.75 4.75736 20.75 6V9V19C20.75 20.2426 19.7426 21.25 18.5 21.25H5.5C4.25736 21.25 3.25 20.2426 3.25 19V9V6C3.25 4.75736 4.25736 3.75 5.5 3.75H7.25V2.75C7.25 2.33579 7.58579 2 8 2ZM8 5.25H5.5C5.08579 5.25 4.75 5.58579 4.75 6V8.25H19.25V6C19.25 5.58579 18.9142 5.25 18.5 5.25H16H8ZM19.25 9.75H4.75V19C4.75 19.4142 5.08579 19.75 5.5 19.75H18.5C18.9142 19.75 19.25 19.4142 19.25 19V9.75Z"
+                  fill="" />
+              </svg>
+              <span class="menu-item-text" :class="sidebarToggle ? 'lg:hidden' : ''">
+                Kategori Kegiatan
+              </span>
+            </a>
+          </li>
+          <li>
+            <a class="menu-item {{ Route::is('user.index') ? 'menu-item-active' : 'menu-item-inactive' }} group"
+              href="{{ route('user.index') }}">
+              <svg class="{{ Route::is('user.index') ? 'menu-item-icon-active' : 'menu-item-icon-inactive' }}"
+                width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd"
+                  d="M8 2C8.41421 2 8.75 2.33579 8.75 2.75V3.75H15.25V2.75C15.25 2.33579 15.5858 2 16 2C16.4142 2 16.75 2.33579 16.75 2.75V3.75H18.5C19.7426 3.75 20.75 4.75736 20.75 6V9V19C20.75 20.2426 19.7426 21.25 18.5 21.25H5.5C4.25736 21.25 3.25 20.2426 3.25 19V9V6C3.25 4.75736 4.25736 3.75 5.5 3.75H7.25V2.75C7.25 2.33579 7.58579 2 8 2ZM8 5.25H5.5C5.08579 5.25 4.75 5.58579 4.75 6V8.25H19.25V6C19.25 5.58579 18.9142 5.25 18.5 5.25H16H8ZM19.25 9.75H4.75V19C4.75 19.4142 5.08579 19.75 5.5 19.75H18.5C18.9142 19.75 19.25 19.4142 19.25 19V9.75Z"
+                  fill="" />
+              </svg>
+              <span class="menu-item-text" :class="sidebarToggle ? 'lg:hidden' : ''">
+                Manajemen User
+              </span>
+            </a>
+          </li>
+        </ul>
+      </div>
     </nav>
   </div>
 </aside>
