087e0dbb91b7e365b2be8726b0bdfb818321bb1e
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2025-11-26 15:15:45 +0700

    feat: apply UserPolicy to User model for enhanced authorization handling

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,14 +2,17 @@
 
 namespace App\Models;
 
+use App\Policies\UserPolicy;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Attributes\Scope;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Attributes\UsePolicy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+#[UsePolicy(UserPolicy::class)]
 class User extends Authenticatable
 {
     use HasFactory, HasUuids;
