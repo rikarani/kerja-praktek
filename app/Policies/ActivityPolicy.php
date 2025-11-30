90599b6a2f16d8105e5bb98f27697f539826e39c
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2025-12-01 06:11:42 +0700

    feat: add delete authorization logic to ActivityPolicy

--- app/Policies/ActivityPolicy.php
+++ app/Policies/ActivityPolicy.php
@@ -4,6 +4,7 @@ namespace App\Policies;
 
 use App\Models\User;
 use App\Models\Activity;
+use Illuminate\Auth\Access\Response;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class ActivityPolicy
@@ -13,5 +14,14 @@ class ActivityPolicy
     public function view(User $user, Activity $activity): bool
     {
         return $user->isAdmin() || $activity->author_id === $user->id;
+    }
+
+    public function delete(User $user, Activity $activity): Response
+    {
+        if ($user->isAdmin() || $activity->author_id === $user->id) {
+            return $this->allow();
+        }
+
+        return $this->deny('You do not have permission to delete this activity');
     }
 }
