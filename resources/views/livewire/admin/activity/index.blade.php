ac78730d16687641e9288bae3b368d70c4d0a29b
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2025-12-01 06:11:28 +0700

    feat: add delete button for activity in index view

--- resources/views/livewire/admin/activity/index.blade.php
+++ resources/views/livewire/admin/activity/index.blade.php
@@ -253,6 +253,21 @@
                     </span>
                   </div>
                 @endif
+                <div class="group relative inline-block">
+                  <button
+                    class="bg-error-500 hover:bg-error-600 shadow-theme-xs inline-flex items-center justify-center gap-2 rounded-lg border border-gray-300 px-4 py-2.5 text-white dark:border-gray-700"
+                    type="button" @click="$dispatch('delete-activity', { activity: '{{ $activity->slug }}'} )">
+                    <svg class="size-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                      stroke-width="1.5" stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round"
+                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                    </svg>
+                  </button>
+                  <span
+                    class="pointer-events-none absolute left-0 top-1/2 mr-2 max-w-max -translate-x-full -translate-y-1/2 rounded-md bg-gray-700 px-3 py-1 text-sm text-white opacity-0 shadow-lg transition-opacity duration-200 group-hover:opacity-100">
+                    Hapus
+                  </span>
+                </div>
               </td>
             </tr>
           @endforeach
@@ -265,6 +280,7 @@
       @endif
       <livewire:modal.admin.activity.publish />
       <livewire:modal.admin.activity.unpublish />
+      <livewire:modal.admin.activity.delete />
     @endif
   </div>
 </div>
