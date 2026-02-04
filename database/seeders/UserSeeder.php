37f1f08deb02d7b4277f718f6b64b813d616ab62
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2026-02-04 09:20:12 +0700

    feat: update UserSeeder to use hardcoded passwords and correct role names

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,7 +6,6 @@ use App\Models\Role;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Config;
 
 class UserSeeder extends Seeder
 {
@@ -14,20 +13,20 @@ class UserSeeder extends Seeder
     {
         // Default Admin User
         User::create([
-            'role_id' => Role::where('name', 'admin')->first()->id,
+            'role_id' => Role::where('name', 'Admin')->first()->id,
             'name' => 'Example Admin',
             'username' => 'admin',
             'email' => fake()->unique()->freeEmail(),
-            'password' => Hash::make(Config::get('app.default_password')),
+            'password' => Hash::make('password'),
         ]);
 
         // Default Regular Users
         User::create([
-            'role_id' => Role::where('name', 'user')->first()->id,
+            'role_id' => Role::where('name', 'User')->first()->id,
             'name' => 'Example User 1',
             'username' => 'ikanteri',
             'email' => fake()->unique()->freeEmail(),
-            'password' => Hash::make(Config::get('app.default_password')),
+            'password' => Hash::make('password'),
         ]);
 
         User::factory(15)->create();
