162719afc457fd002b210bea202c04741a45699f
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2026-03-14 23:50:52 +0700

    feat: update create.blade.php to rename label from 'Kata-Kata' to 'Ringkasan Kegiatan'

--- resources/views/livewire/admin/activity/create.blade.php
+++ resources/views/livewire/admin/activity/create.blade.php
@@ -97,7 +97,7 @@
   <div>
     <label class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400" wire:loading.class="opacity-50"
       wire:target="saveAsDraft, saveAndPublish">
-      Kata-Kata <span class="text-red-500">*</span>
+      Ringkasan Kegiatan <span class="text-red-500">*</span>
     </label>
     <textarea
       class="{{ twMerge(['disabled:opacity-50 shadow-theme-xs focus:border-brand-300 focus:ring-brand-500/10 dark:focus:border-brand-800 focus:ring-3 focus:outline-hidden w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 placeholder:text-gray-400 dark:bg-gray-900 dark:border-gray-700 dark:text-white/90 dark:placeholder:text-white/30', $errors->has('description') ? 'border-error-300 focus:border-error-300 focus:ring-error-500/10 dark:border-error-700 dark:focus:border-error-800' : '']) }}"
