4e2496bb7546718bc7398f57e983b4bc8736ee77
rikarani <46773591+rikarani@users.noreply.github.com>
2026-04-05 16:49:16 +0700

    feat: update edit profile page to use 'Profil' instead of 'Profile'

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -3,7 +3,7 @@
 @section('main-content')
   <div class="rounded-2xl border border-gray-200 bg-white p-5 lg:p-6 dark:border-gray-800 dark:bg-white/[0.03]">
     <h3 class="mb-5 text-lg font-semibold text-gray-800 lg:mb-7 dark:text-white/90">
-      Profile
+      Profil
     </h3>
     <div class="mb-6 rounded-2xl border border-gray-200 p-5 lg:p-6 dark:border-gray-800">
       <div class="flex flex-col gap-5 xl:flex-row xl:items-center xl:justify-between">
