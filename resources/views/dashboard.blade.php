52b408eee1b5b67644ddceb789f9841b917608b0
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2026-04-11 03:07:30 +0700

    feat: add dashboard view with yearly and monthly charts

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')
+
+@section('main-content')
+  <div class="grid grid-cols-12 gap-4 md:gap-6">
+    <div class="col-span-12 space-y-6 xl:col-span-6">
+      <livewire:chart.per-year />
+    </div>
+    <div class="col-span-12 space-y-6 xl:col-span-6">
+      <livewire:chart.per-month />
+    </div>
+  </div>
+@endsection
