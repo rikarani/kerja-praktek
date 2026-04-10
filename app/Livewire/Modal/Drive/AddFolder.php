01f387987a3190a5a72dad367a56436c2b7c6dc0
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2026-04-11 03:06:50 +0700

    feat: add modals and components for adding documentations and folders, and renaming folders

---
+++ app/Livewire/Modal/Drive/AddFolder.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Livewire\Modal\Drive;
+
+use Livewire\Component;
+use App\Models\Activity;
+use Livewire\Attributes\On;
+use Livewire\Attributes\Url;
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Storage;
+
+class AddFolder extends Component
+{
+    public ?Activity $activity = null;
+
+    public string $title = '';
+
+    #[Url]
+    public ?string $path = null;
+
+    public function mount(Activity $activity): void
+    {
+        $this->activity = $activity;
+    }
+
+    #[On('add-folder')]
+    public function prepare(): void
+    {
+        $this->dispatch('open-modal', modal: 'add-folder');
+    }
+
+    public function addFolder(): void
+    {
+        $data = $this->validate();
+
+        if (filled($this->path)) {
+            Storage::disk('google')->makeDirectory("{$this->activity->year}/{$this->activity->title}/$this->path/{$data['title']}");
+
+            $this->redirect("/kegiatan/{$this->activity->slug}/drive?path=$this->path");
+        } else {
+            Storage::disk('google')->makeDirectory("{$this->activity->year}/{$this->activity->title}/{$data['title']}");
+
+            $this->redirectRoute('activity.drive', ['activity' => $this->activity]);
+        }
+
+        $this->dispatch('close-modal');
+    }
+
+    protected function rules(): array
+    {
+        return [
+            'title' => 'required|string',
+        ];
+    }
+
+    public function render(): View
+    {
+        return view('livewire.modal.drive.add-folder');
+    }
+}
