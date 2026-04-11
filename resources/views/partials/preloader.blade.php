090bdf8846b98d8424147534d61886dec087b75a
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2026-04-11 20:57:53 +0700

    apus

--- resources/views/partials/preloader.blade.php
+++
@@ -1,3 +0,0 @@
-<div x-show="loaded" x-init="window.addEventListener('DOMContentLoaded', () => { setTimeout(() => loaded = false, 500) })" class="z-999999 fixed left-0 top-0 flex h-screen w-screen items-center justify-center bg-white dark:bg-black">
-  <div class="border-brand-500 h-16 w-16 animate-spin rounded-full border-4 border-solid border-t-transparent"></div>
-</div>
