9ded018aed6f19f6805a9cd31da53a91f0225920
rikarani <46773591+rikarani@users.noreply.github.com>
2026-04-27 21:58:26 +0700

    feat: enhance download and delete methods in ActivityFileController to support folder-based file operations

--- app/Http/Controllers/ActivityFileController.php
+++ app/Http/Controllers/ActivityFileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Support\Helper;
 use App\Models\Activity;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Storage;
 use Symfony\Component\HttpFoundation\StreamedResponse;
@@ -12,14 +13,29 @@ class ActivityFileController extends Controller
 {
     public function download(Activity $activity, string $path): StreamedResponse
     {
+        parse_str(parse_url(URL::previous(), PHP_URL_QUERY), $query);
+
+        if (! empty($query['path'])) {
+            $folder = $query['path'];
+
+            return Storage::disk('google')->download("{$activity->year}/{$activity->title}/$folder/$path");
+        }
+
         return Storage::disk('google')->download("{$activity->year}/{$activity->title}/$path");
     }
 
     public function delete(Activity $activity, string $path): RedirectResponse
     {
+        parse_str(parse_url(URL::previous(), PHP_URL_QUERY), $query);
+
+        if (! empty($query['path'])) {
+            $folder = $query['path'];
+            Storage::disk('google')->delete("{$activity->year}/{$activity->title}/$folder/$path");
+        }
+
         Storage::disk('google')->delete("{$activity->year}/{$activity->title}/$path");
         Helper::invalidateDocumentationCache($activity);
 
-        return to_route('activity.drive', ['activity' => $activity]);
+        return back();
     }
 }
